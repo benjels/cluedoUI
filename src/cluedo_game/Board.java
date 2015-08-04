e739c59e0af2b011d641fa17253ff301f89223b0
Max <maxbrown0101@gmail.com>
2015-08-04 23:25:06 +1200

    added some more to game/player.

--- src/cluedo_game/Board.java
+++ src/cluedo_game/Board.java
@@ -3,9 +3,14 @@ package cluedo_game;
 import java.io.File;
 import java.util.ArrayList;
 
+import cluedo_game.Board.Direction;
 import cluedo_game.CharacterCard.Character;
 
 public class Board {
+
+	public enum Direction {
+
+	}
 
 	public Board(File file, ArrayList<Player> players) {
 		// TODO Auto-generated constructor stub
@@ -21,6 +26,20 @@ public class Board {
 	public int charToY(Character playersChar) {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+
+
+
+	public boolean canMove(PlayerTile movingTile, Direction dir) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+
+	public PlayerTile move(PlayerTile movingTile, Direction dir) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- src/cluedo_game/Game.java
+++ src/cluedo_game/Game.java
@@ -101,7 +101,7 @@ public class Game {
 		while(!gameOver){
 			for(Player eachPlayer: this.players){
 				//first roll the dice
-				
+				int diceRoll = 
 				//pass diceroll to player's haveMove method which passes moves through
 				//this class' sendMove method
 				
@@ -139,6 +139,63 @@ public class Game {
 	// !!!REMEMBER THAT THERE ARE 2 GAME OVER CONDITIONS
 	// 1) A PLAYER GUESSES CORRECT CARD COMBINATION
 	// 2) ALL PLAYERS HAVE GUESSED WRONGLY
+	
+	
+	/**
+	 * when a player pushes a key to move somewhere, move is sent to this method
+	 * . This method checks with the board to see if the move is valid. IF it not, this method returns to the decideMove
+	 * method the same tile as before. If it is valid, it moves the tile on the board and returns this
+	 * new version of the tile to the player
+	 * @param movingTile the tile of the player who is attempting to move
+	 * @param dir the direction that the player is trying to move in
+	 * @return the player's tile after this attempted move
+	 */
+	public PlayerTile sendMove(PlayerTile movingTile, Board.Direction dir){
+		//check with the canmove whether the desired move is valid
+		if(!this.board.canMove(movingTile, dir)){
+			//in the case that we can't make the move because of e.g. a wall
+			return movingTile;
+		}else{//in the case that requested move is valid
+			return this.board.move(movingTile, dir);
+		}
+	}
+	
+	/**
+	 * when a player makes a guess or an accusation it is passed to here
+	 * so that it can be given to all the players so that they can potentially
+	 * refute this guess. From here, the players 
+	 * @param guess
+	 */
+	public void checkGuess(Guess guess){
+		
+		
+		//get the first player in the list who can refute
+		Player refuter = null;
+		for(Player eachPlayer: this.players){
+			if(eachPlayer.canRefute(guess)){
+				refuter = eachPlayer;
+				break;
+			}
+		}
+		//deal with case where guess was correct/nobody can refute
+		if(refuter == null){
+			if(guess.isFinal){//then the guessing player just won the game
+				win game
+			}else{
+				System.out.println("no players can refute this combination");
+			}
+		}else{//in likely case that at least one player can refute the guess
+			//get the cards that they could refute with
+			HashSet<? extends Card> refutationCards = refuter.getPotentialRefutationCards(guess);
+			need to decide here whether should print out possible refutes here or pass the refutation cards to
+			some new method in the refuting player so that input is done there and then passed back to here
+			then imo just insert the method calls (all the game logic is now delegated to these hlper method) into
+			the rungame method and start working on all the github issues then start debug
+		}
+		
+	}
+	
+	
 
 	public static void main(String[] args) {
 		System.out.println("making a new game...");
